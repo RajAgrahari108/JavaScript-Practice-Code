0e418711626d0cad1ab2d13af17a308f1365a7ae
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-18 22:46:29 +0530

    Adding

--- JavaScript Practice Code/Day 4 (String)/app.js
+++ JavaScript Practice Code/Day 4 (String)/app.js
@@ -9,4 +9,18 @@ console.log(str.substring(2,10));
 console.log(str.split(' '));
 
 
+//  !12. replace('prevSubst' , 'newSubst')
+// it will replace the first occurence of a substring with a new substing
+// let str3 = 'HAHAHAHAHAHEHEHEHEHE'
+// let temp = str.replace('HA' , 'HE')
+// console.log(temp);
+// !12. replaceAll('prevSubst' , 'newSubst')
+// it will replace the all tge occurence of a substring with a new substing
+let str3 = 'HAHAHAHAHAHEHEHEHEHE'
+let temp = str3.replaceAll('HA' , 'HE')
+let temp1 = str3.replaceAll('HE' , 'HA')
+console.log(temp);
+console.log(temp1);
+
+
  
