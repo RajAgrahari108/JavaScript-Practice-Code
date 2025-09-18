fd1674397ac405b01b54ea588c09c560f42febe6
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-18 19:11:32 +0530

    adding

---
+++ JavaScript Practice Code/03_Basic (peactice)/app.js
@@ -0,0 +1,48 @@
+// let a =10;
+// let a = 23;
+// var x=20;
+// var x=20
+// var x=30;
+// console.log(x);
+
+// const score = 100; //number
+// const username = "Alert"; //string
+// const isAdmin = true; //boolen
+// let notAssigned; //underfined
+// const empty = null; // null
+// const unique = symbole(); //symbole
+// const big = 12344567890; //bigint
+
+
+
+// let a =10;
+// let b = 23;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a ** b);
+// console.log(a / b); 
+
+
+
+// ! print your name
+// const name = "Raj Agrahari"
+// let age  = 21;
+
+// console.log(`My Name is ${name} and i am ${age} year old.`)
+
+// ! area of rectangel 
+// let width = 12;
+// let hight = 23;
+// let area = width*hight;
+// console.log(area);
+
+
+// ! Write a program that converts Celsius → Fahrenheit. Formula: (C × 9/5) + 32
+
+// let C = 37;
+// let f = (C * 9/5) + 32
+// console.log("The Fahrenheit of given Celsius is: " + f);
+
---
+++ JavaScript Practice Code/Day 4 (String)/app.js
@@ -0,0 +1,12 @@
+// ! substring
+
+
+let str = 'Raj Agrahari';
+let str2 = 'Panagarh';
+console.log(str.length);
+console.log(str.substring(2,10));
+
+console.log(str.split(' '));
+
+
+ 
