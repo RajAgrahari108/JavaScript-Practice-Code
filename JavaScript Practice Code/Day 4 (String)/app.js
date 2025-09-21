410716e786e7eac8c2b568c1cf637848fde849ed
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-21 17:48:42 +0530

    Adding

--- JavaScript Practice Code/Day 4 (String)/app.js
+++ JavaScript Practice Code/Day 4 (String)/app.js
@@ -1,12 +1,12 @@
 // ! substring
 
 
-let str = 'Raj Agrahari';
-let str2 = 'Panagarh';
-console.log(str.length);
-console.log(str.substring(2,10));
+// let str = 'Raj Agrahari';
+// let str2 = 'Panagarh';
+// console.log(str.length);
+// console.log(str.substring(2,10));
 
-console.log(str.split(' '));
+// console.log(str.split(' '));
 
 
 //  !12. replace('prevSubst' , 'newSubst')
@@ -16,11 +16,23 @@ console.log(str.split(' '));
 // console.log(temp);
 // !12. replaceAll('prevSubst' , 'newSubst')
 // it will replace the all tge occurence of a substring with a new substing
-let str3 = 'HAHAHAHAHAHEHEHEHEHE'
-let temp = str3.replaceAll('HA' , 'HE')
-let temp1 = str3.replaceAll('HE' , 'HA')
-console.log(temp);
-console.log(temp1);
+// let str3 = 'HAHAHAHAHAHEHEHEHEHE'
+// let temp = str3.replaceAll('HA' , 'HE')
+// let temp1 = str3.replaceAll('HE' , 'HA')
+// console.log(temp);
+// console.log(temp1);
 
+
+// ! collback function 
+
+function sum(p1 , p2){
+    console.log(p1+p2); 
+}
+function sub(p1 , p2){
+    console.log(p1-p2); 
+}
+function mult(p1 , p2){
+    console.log(p1*p2); 
+}
 
  
