12c5ade5104d3320f70301865c21fcd3889045c6
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-22 02:15:14 +0530

    Adding

--- JavaScript Practice Code/day 5(Array)/array.js
+++ JavaScript Practice Code/day 5(Array)/array.js
@@ -1,6 +1,12 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
-let arr2 = ["Raj", "dhru","Ravi", "Papa", "maa"];
-const myArray = new Array(1, 2, 3, );
-console.log(arr);
-console.log(arr2);
-console.log(myArray);
+let myArr = [1, 2, 3, 4, 5, 6, 7];
+// let myArray1 = ["Raj", "dhru","Ravi", "Papa", "maa"];
+// const myArray2 = new Array(1, 2, 3, );
+// console.log(myArr);
+// console.log(myArray1);
+// console.log(myArray2);
+
+
+// ! push 
+
+myArr.push(8);
+console.log(myArr);
