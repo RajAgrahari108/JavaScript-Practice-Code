0da1ad40ecf1367a4b1bc053704efd6b3815eed7
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-25 01:12:00 +0530

    Adding

--- JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
+++ JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
@@ -1,7 +1,11 @@
 console.log(Math);
 
 // ! Properties
-console.log(Math.PI); // 3.14159
-console.log(Math.SQRT2);
-console.log(Math.SQRT1_2);
+// console.log(Math.PI); // 3.14159
+// console.log(Math.SQRT2);
+// console.log(Math.SQRT1_2);
 
+// ! Math.abs()
+// convert all the values to the absolute value
+console.log(Math.abs(-40)); // 40
+console.log(Math.abs(40)); // 40
