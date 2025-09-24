226166f02cbbedc2af5c530f35067bec321dc07d
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-25 01:12:54 +0530

    Adding

--- JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
+++ JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
@@ -9,3 +9,9 @@ console.log(Math);
 // convert all the values to the absolute value
 console.log(Math.abs(-40)); // 40
 console.log(Math.abs(40)); // 40
+
+
+// ! Math.floor()
+// it will return the nearest lowest integer value
+console.log(Math.floor(20.9)); // 20
+console.log(Math.floor(30.5)); // 30
---
+++ JavaScript Practice Code/Day 8 (.Math & Number's)/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+console.log(Math.floor(20.9)); // 20
+console.log(Math.floor(30.5)); // 30
