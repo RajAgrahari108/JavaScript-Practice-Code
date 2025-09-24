c7211ecbc31dbd563e37cf08326dbc786148d16f
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-25 01:13:52 +0530

    Adding

--- JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
+++ JavaScript Practice Code/Day 8 (.Math & Number's)/app.js
@@ -13,5 +13,14 @@ console.log(Math.abs(40)); // 40
 
 // ! Math.floor()
 // it will return the nearest lowest integer value
-console.log(Math.floor(20.9)); // 20
-console.log(Math.floor(30.5)); // 30
+// console.log(Math.floor(20.9)); // 20
+// console.log(Math.floor(30.5)); // 30
+
+
+
+
+// ! Math.ceil()
+// it will return the nearest highest integer value
+console.log(Math.ceil(20.9)); // 21
+console.log(Math.ceil(30.1)); // 31
+console.log(Math.ceil(30.4)); // 31
