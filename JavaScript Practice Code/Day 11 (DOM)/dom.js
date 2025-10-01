49be8941d49cacf071846a4af5e25c712906bbfb
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-01 23:35:05 +0530

    adding event listener in dom

--- JavaScript Practice Code/Day 11 (DOM)/dom.js
+++ JavaScript Practice Code/Day 11 (DOM)/dom.js
@@ -1,5 +1,11 @@
 let v = document.querySelector("h1")
 
-v.innerHTML= 'heheheh'
- v.style.color= "red";
- v.style.backgroundColor = 'black'
+// v.innerHTML= 'heheheh'
+//  v.style.color= "red";
+//  v.style.backgroundColor = 'black'
+
+v.addEventListener("click" ,function(){
+    v.innerHTML = 'hello javascript'
+    v.style.color = 'blue'
+    v.style.backgroundColor = "black"
+})
