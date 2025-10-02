dbc1d297f4d914eb3ddc5e840fe15ed39e374123
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-02 22:22:10 +0530

    Create a on of button for bulb using html,css and javascript

--- JavaScript Practice Code/Day 11 (DOM)/dom.js
+++ JavaScript Practice Code/Day 11 (DOM)/dom.js
@@ -15,5 +15,5 @@ var bulb = document.querySelector("#bulb")
 var btn = document.querySelector("button")
 var flag = 0;
 btn.addEventListener("click" , function(){
-    bulb.style.backgroundColor= "yellow"
+    bulb.style.backgroundColor= "ellow"
 });
