8f478428030c44c18cdb7e82c415c8998bede204
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-21 22:13:37 +0530

    practice code of javscript

---
+++ Akshay sir class code only javascript/Chatgpt Practice code/three/app.js
@@ -0,0 +1,41 @@
+// function greet(){
+//     console.log("hello js");
+    
+// }
+// greet();
+
+
+
+// function sum(a, b){
+//     return a+b;
+// }
+// console.log(sum(10,20));
+
+// function product(a, b){
+//     return a*b;
+// }
+// console.log(product(1,20));
+
+// function diff(a, b){
+//     return a-b;
+// }
+// console.log(diff(111,20));
+
+
+// var x = 5;
+// let y = 10;
+// const z = 15;
+// if(true){
+//     var x = 20;
+//     let y = 25;
+//     const z = 30;
+//     console.log(x, y, z);
+// }
+// console.log(x, y, z);
+  
+
+let show  = () => console.log("hello");
+    
+
+
+
