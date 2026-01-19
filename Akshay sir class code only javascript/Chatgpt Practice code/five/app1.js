49ed937628c571b550abf8646298094a28aac6a3
Raj Agrahari <raj.agr2002@gmail.com>
2026-01-19 23:57:51 +0530

    code submit fir prictice

---
+++ Akshay sir class code only javascript/Chatgpt Practice code/five/app1.js
@@ -0,0 +1,11 @@
+// const myNum = [1, 2,3, 3, 4,5,6,,7,8, 9]
+
+// const newNum = myNum.filter((item)=>{
+//     return item<5;
+// })
+// console.log(newNum);
+
+let allProduct = Map.detailedProducts
+
+
+
