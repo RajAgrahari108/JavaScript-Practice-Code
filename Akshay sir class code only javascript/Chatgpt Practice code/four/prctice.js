d705806f6458523602c3ac03eced5435ae75ae70
Raj Agrahari <raj.agr2002@gmail.com>
2026-01-25 23:54:28 +0530

    code submit for pracctice

---
+++ Akshay sir class code only javascript/Chatgpt Practice code/four/prctice.js
@@ -0,0 +1,16 @@
+
+let p1 = {
+    name:'Demo',
+    age:40
+}
+let p2 = {
+    name:'Dinga',
+    age:50
+}
+fun.call(p1 , 20)
+// fun.call(p2 , 70)
+// fun.apply(p2 , [90])
+// let res = fun.bind(p1)
+// res(10)
+// res(20)
+// res(30)
