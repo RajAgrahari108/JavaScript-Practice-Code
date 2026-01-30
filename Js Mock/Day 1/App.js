bf3b0d1e86497b108a2e97d6ce4c4a8deddf94e0
Raj Agrahari <raj.agr2002@gmail.com>
2026-01-31 00:03:29 +0530

    code for submiting

--- Js Mock/Day 1/App.js
+++ Js Mock/Day 1/App.js
@@ -23,11 +23,41 @@
 
 
 
-async function getData(){
-        let a = await fetch('https://jsonplaceholder.typicode.com/posts')
-        let b= await a.json();
-    console.log(a);
-}
-getData();
+// async function getData(){
+//         let a = await fetch('https://jsonplaceholder.typicode.com/posts')
+//         let b= await a.json();
+//     console.log(a);
+// }
+// getData();
 
 
+// let prices = [100, 200, 300];
+// let gst = prices.map((el)=>{
+//     return el+el*0.18;
+// })
+// console.log(gst);
+
+
+let prices = [100, 200, 300];
+let gst = prices.map(el=>el+el*0.18);
+// console.log(gst);
+
+
+let ages = [12, 18, 25, 10, 30];
+let AG= ages.filter(el=>el>=18);
+// console.log(AG);
+
+
+let num = [10,12, 18, 25, 10, 30];
+let sum = num.reduce((acc, el)=>{
+    return acc + el;
+},0);
+
+// console.log(sum);
+
+
+let marks = [80, 70, 90];
+let marksSum = marks.reduce((acc, el)=>{
+    return acc +  el;
+},0)
+console.log(marksSum);
